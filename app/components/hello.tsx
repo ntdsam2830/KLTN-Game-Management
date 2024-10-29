53f5fb3ac308d09241e1fdb13845d309ad94a7aa
ntdsam2830 <ntdsam@gmail.com>
2024-10-29 12:24:26 +0700

    set up

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Hello() {
+  console.log("Hi there");
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+export default Hello;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Hello from "./components/hello";
+
 export default function Home() {
+  console.log("Hello");
+
   return (
     <div>
       <h1 className="text-3xl underline">Home</h1>
+      <Hello />
     </div>
   );
 }
